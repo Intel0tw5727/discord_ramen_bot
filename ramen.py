b125a7c67669f4e8d25d1d9faf7a837b4cd33a9d
Intel0tw5727 <intel0tw5727@gmail.com>
2018-05-05 00:53:29 +0900

    [update] 読み出しをwith openからpandasへ変更

--- ramen.py
+++ ramen.py
@@ -2,13 +2,11 @@
 read_ramen.py
 ラーメン店舗の追加や削除などを行うモジュール
 """
+import pandas as pd
 
-def read_ramen():
-    ramens = []
-    with open("ramen_list.txt", "r") as f:
-        for r in f.readlines():
-            ramens.append(r.replace("\n", ""))
-    return ramens
+def read_ramens():
+    df = pd.read_csv("ramen_data.csv")
+    return df
 
 if __name__ == "__main__":
-    print(read_ramen())
+    print(read_ramens())
